c6c9192dff5901e803c696d2161076955e3f40d7
Miftah GanzZ <138507664+miftahganzz@users.noreply.github.com>
2024-03-05 19:50:59 +0700

    File Uploader

---
+++ index.js
@@ -0,0 +1,212 @@
+const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+const swaggerUi = require("swagger-ui-express");
+
+const app = express();
+const port = 3000;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "file/");
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(6, (err, raw) => {
+      if (err) return cb(err);
+      cb(null, raw.toString("hex") + path.extname(file.originalname));
+    });
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.use(express.static("public"));
+app.use('/file', express.static(path.join(__dirname, 'file')));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/privacy-policy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy.html"));
+});
+
+app.post("/upload", upload.single("file"), (req, res) => {
+  const fileName = req.file.filename;
+  const fileUrl = `http://${req.hostname}/file/${fileName}`;
+  const downloadUrl = `http://${req.hostname}/download/${fileName}`;
+  const deleteUrl = `http://${req.hostname}/delete/${fileName}`;
+
+  const fileDetails = {
+    fileName: fileName,
+    originalName: req.file.originalname,
+    size: req.file.size,
+    extension: path.extname(req.file.originalname),
+    uploadTime: new Date().toISOString(),
+  };
+
+  const responseData = {
+    fileDetails: fileDetails,
+    fileUrl: fileUrl,
+    downloadUrl: downloadUrl,
+    deleteUrl: deleteUrl,
+    message: "File uploaded successfully",
+  };
+  res.json(responseData);
+});
+
+app.get("/download/:fileName", (req, res) => {
+  const fileName = req.params.fileName;
+  const filePath = path.join(__dirname, "file", fileName);
+
+  if (fs.existsSync(filePath)) {
+    res.download(filePath, fileName, (err) => {
+      if (err) {
+        console.error("Error downloading file:", err);
+        res.status(500).send("Error downloading file");
+      }
+    });
+  } else {
+    res.status(404).send("File not found");
+  }
+});
+
+app.delete("/delete/:fileName", (req, res) => {
+  const fileName = req.params.fileName;
+  const filePath = path.join(__dirname, "file", fileName);
+
+  if (fs.existsSync(filePath)) {
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error("Error deleting file:", err);
+        res.status(500).send("Error deleting file");
+        return;
+      }
+      res.send("File deleted successfully");
+    });
+  } else {
+    res.status(404).send("File not found");
+  }
+});
+
+var options = {
+    customSiteTitle: "ITzpire Uploader",
+    customfavIcon: "https://i.ibb.co/GphSk0T/upload.png",
+    customCss: `.topbar { display: none; }`,
+    swaggerOptions: {
+      displayRequestDuration: true,
+    },
+  };
+
+const swaggerDocument = {
+  swagger: "2.0",
+  info: {
+    version: "1.0.0",
+    title: "Uploader Docs",
+    description: "Docs for Uploader",
+    "x-logo": {
+      url: "https://i.ibb.co/GphSk0T/upload.png",
+      altText: "ITzpire Upload",
+    },
+  },
+  host: "cdn.itzpire.site",
+  basePath: "/",
+  tags: [{ name: "Upload", description: "Endpoints related to file upload" }],
+  paths: {
+    "/upload": {
+      post: {
+        tags: ["Upload"],
+        summary: "Upload a file",
+        consumes: ["multipart/form-data"],
+        parameters: [
+          {
+            in: "formData",
+            name: "file",
+            type: "file",
+            description: "File to upload",
+          },
+        ],
+        responses: {
+          200: {
+            description: "File uploaded successfully",
+            schema: {
+              type: "object",
+              properties: {
+                fileName: {
+                  type: "string",
+                  description: "The name of the uploaded file",
+                },
+                fileUrl: {
+                  type: "string",
+                  description: "The URL of the uploaded file",
+                },
+                message: {
+                  type: "string",
+                  description: "A success message",
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+    "/download/{fileName}": {
+      get: {
+        tags: ["Upload"],
+        summary: "Download a file",
+        parameters: [
+          {
+            in: "path",
+            name: "fileName",
+            type: "string",
+            required: true,
+            description: "The name of the file to download",
+          },
+        ],
+        responses: {
+          200: {
+            description: "File downloaded successfully",
+            schema: {
+              type: "file",
+            },
+          },
+          404: {
+            description: "File not found",
+          },
+        },
+      },
+    },
+    "/delete/{fileName}": {
+      delete: {
+        tags: ["Upload"],
+        summary: "Delete a file",
+        parameters: [
+          {
+            in: "path",
+            name: "fileName",
+            type: "string",
+            required: true,
+            description: "The name of the file to delete",
+          },
+        ],
+        responses: {
+          200: {
+            description: "File deleted successfully",
+          },
+          404: {
+            description: "File not found",
+          },
+        },
+      },
+    },
+  },
+  "x-request-time": new Date().toISOString(),
+};
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
+
+app.listen(port, () => {
+  console.log(`Server is listening at http://localhost:${port}`);
+});
---
+++ public/script.js
@@ -0,0 +1,148 @@
+document.getElementById("fileInput").addEventListener("change", function () {
+  const fileLabel = document.getElementById("fileLabel");
+  const fileInput = document.getElementById("fileInput");
+  if (fileInput.files.length > 0) {
+      fileLabel.textContent = `File selected: ${fileInput.files[0].name}`;
+      showProgressBar();
+  } else {
+      fileLabel.textContent = "Choose a file or drag it here";
+      hideProgressBar();
+  }
+});
+
+function showProgressBar() {
+  document.getElementById("progressContainer").classList.remove("hidden");
+}
+
+function hideProgressBar() {
+  document.getElementById("progressContainer").classList.add("hidden");
+}
+
+document.getElementById("btnUpload").addEventListener("click", function () {
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+  if (!file) {
+      return;
+  }
+  const formData = new FormData();
+  formData.append("file", file);
+  const progressBar = document.getElementById("progressBar");
+  const progressText = document.getElementById("progressText");
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", "/upload", true);
+  xhr.upload.onprogress = function (event) {
+      const percent = (event.loaded / event.total) * 100;
+      progressBar.style.width = percent + "%";
+      progressText.textContent = percent.toFixed(0) + "%";
+  };
+  xhr.onload = async function () {
+      if (xhr.status === 200) {
+          await Swal.fire({
+              icon: "success",
+              title: "Success",
+              text: "File uploaded successfully",
+          });
+          displayFileDetails(JSON.parse(xhr.responseText));
+      } else {
+          await Swal.fire({
+              icon: "error",
+              title: "Error",
+              text: "Failed to upload file",
+          });
+      }
+  };
+  xhr.onerror = async function () {
+      await Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "An error occurred while uploading file",
+      });
+  };
+  xhr.send(formData);
+});
+
+function displayFileDetails(responseData) {
+  const fileDetails = responseData.fileDetails;
+  const fileUrl = responseData.fileUrl;
+  const downloadUrl = responseData.downloadUrl;
+  const deleteUrl = responseData.deleteUrl;
+  const totalFiles = responseData.totalFiles;
+  const totalGB = responseData.totalGB;
+
+  const details = document.getElementById("details");
+  details.innerHTML = `
+      <p>File Name: ${fileDetails.fileName}</p>
+      <p>Original Name: ${fileDetails.originalName}</p>
+      <p>File Size: ${formatBytes(fileDetails.size)}</p>
+      <p>Extension: ${fileDetails.extension}</p>
+      <p>Upload Time: ${fileDetails.uploadTime}</p>
+  `;
+
+  const fileDetailsContainer = document.getElementById("fileDetails");
+  fileDetailsContainer.classList.remove("hidden");
+  fileDetailsContainer.classList.add("animate__animated", "animate__fadeIn");
+
+  const urlContainer = document.querySelector(".url-container");
+  urlContainer.classList.remove("hidden");
+  const fileValue = document.getElementById("fileValue");
+  const downloadValue = document.getElementById("downloadValue");
+  const deleteValue = document.getElementById("deleteValue");
+  const totalFileValue = document.getElementById("totalFileValue");
+  const totalGBValue = document.getElementById("totalGBValue");
+  fileValue.textContent = fileUrl;
+  downloadValue.textContent = downloadUrl;
+  deleteValue.textContent = deleteUrl;
+  totalFileValue.textContent = totalFiles;
+  totalGBValue.textContent = totalGB;
+
+  const preview = document.getElementById("preview");
+  preview.innerHTML = "";
+  const fileInput = document.getElementById("fileInput");
+  const file = fileInput.files[0];
+  if (file) {
+      const imageTypes = ["image/jpeg", "image/png", "image/gif"];
+      if (imageTypes.includes(file.type)) {
+          const img = document.createElement("img");
+          img.src = URL.createObjectURL(file);
+          img.alt = "File Preview";
+          img.className = "max-w-full h-auto mx-auto";
+          preview.appendChild(img);
+      } else {
+          const fileType = document.createElement("p");
+          fileType.textContent = `File Type: ${file.type}`;
+          preview.appendChild(fileType);
+      }
+  }
+}
+
+function formatBytes(bytes, decimals = 2) {
+  if (bytes === 0) return "0 Bytes";
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
+}
+
+function copyToClipboard(valueId) {
+  const value = document.getElementById(valueId).textContent;
+  try {
+      navigator.clipboard.writeText(value);
+      Swal.fire({
+          icon: "success",
+          title: "Copied!",
+          text: "URL copied to clipboard",
+      });
+  } catch (error) {
+      console.error("Error copying to clipboard:", error);
+      Swal.fire({
+          icon: "error",
+          title: "Error",
+          text: "Failed to copy URL to clipboard",
+      });
+  }
+}
+
+document.getElementById("btnCopyFile").addEventListener("click", () => copyToClipboard("fileValue"));
+document.getElementById("btnCopyDownload").addEventListener("click", () => copyToClipboard("downloadValue"));
+document.getElementById("btnCopyDelete").addEventListener("click", () => copyToClipboard("deleteValue"));
